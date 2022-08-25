4024d9fb3284fe394dc7ad9a42458b5ba26a4ff1
Albert K <mralbertkonrad@gmail.com>
2022-08-25 14:24:04 +0200

    refactor: streamlined aggregator class

--- tracker/src/Aggregate.scala
+++ tracker/src/Aggregate.scala
@@ -6,53 +6,20 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 
 case class Aggregate(spark: SparkSession) {
 
-  def buildAggregate(in: String, out: String): Any = {
+  def createAggregate(filePath: String, fileName: String) = {
+    
+    val data = DataLoaders.LoadCovidData(spark).byCity(filePath)
 
-    def writeData(ds: DataFrame, temp: String) = {
-      ds.createOrReplaceTempView("CovidDataByCity")
-      val dataByRegion = spark.sql("""
-        SELECT date
-        , state
-        , SUM(cases) as cases
-        , SUM(deaths) as deaths 
+    data.createOrReplaceTempView("CovidDataByCity")
+    
+    val dataByState = spark.sql("""
+        SELECT date, state, SUM(cases) as cases, SUM(deaths) as deaths 
         FROM CovidDataByCity 
         GROUP BY date, state 
         ORDER BY date, state DESC
-        """)
-      dataByRegion
-        .coalesce(1)
-        .write
-        .option("header", true)
-        .mode("OVERWRITE")
-        .csv(temp)
-    }
+        """)  
 
-    def renameOutput(temp: String, outPath: String, out: String) = {
-      val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
-      val fileName = fs.globStatus(new Path(s"$temp/*.csv"))(0).getPath().getName()
-      fs.rename(new Path(temp + "/" + fileName), new Path(s"$outPath/$out"))
-    }
-
-    def cleanup(temp: String, outPath: String) = {
-      
-      import scala.reflect.io.Directory
-      import java.io.File
-      
-      val tempDir = new Directory(new File(temp))
-      tempDir.deleteRecursively()
-      for {
-        files <- Option(new File(outPath).listFiles)
-        file <- files if file.getName.endsWith("crc")
-      } file.delete()
-
-    }
-  
-    val outPath = "./data/out/"
-    val tempPath = "./data/temp/"
-    val data = DataLoaders.LoadCovidData(spark).byCity(in)
-    writeData(data, tempPath)
-    renameOutput(tempPath, outPath, out)
-    cleanup(tempPath, outPath)
+    DataWriters.WriteCsvFile(spark).writeFile(dataByState, fileName)
   }
 
 }
--- tracker/src/TrackerCli.scala
+++ tracker/src/TrackerCli.scala
@@ -6,7 +6,7 @@ object TrackerCli {
   (2) [Compare two aggregations] cmp <path/to/aggregate1> <path/to/aggregate2>"""
 
   def main(args: Array[String]): Unit = args match {
-    case Array("agg", in, out) => SparkContext.runWithSpark(Aggregate(_).buildAggregate(in, out))
+    case Array("agg", in, out) => SparkContext.runWithSpark(Aggregate(_).createAggregate(in, out))
     case Array("cmp", left, right) => SparkContext.runWithSpark(Compare(_).compare(left, right))
     case _ => println(errorMsg)
   }
